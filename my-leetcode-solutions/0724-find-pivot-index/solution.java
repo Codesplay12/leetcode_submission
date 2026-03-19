188cc2c298bb64f5ef84524796e3473c65505a8b
Harsh jain <144236550+Codesplay12@users.noreply.github.com>
2026-03-19 18:15:15 +0000

    Sync LeetCode submission Runtime - 1 ms (98.58%), Memory - 47.6 MB (8.68%)

---
+++ my-leetcode-solutions/0724-find-pivot-index/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int left = 0;
+        int sum = 0;
+        int right;
+
+        for(int i = 0;i<nums.length;i++){
+            sum += nums[i];
+        }
+
+        for(int i = 0;i<nums.length;i++){
+           
+            right = sum - nums[i] - left;
+            if(left == right){
+                return i;
+            }
+            left = left + nums[i];
+
+
+        }
+        return -1;
+    }
+}
