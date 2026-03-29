e7740dbf0958ec972c1a9ebdb739b0f94dcf9938
Harsh jain <144236550+Codesplay12@users.noreply.github.com>
2026-03-29 08:14:09 +0000

    Sync LeetCode submission Runtime - 675 ms (71.44%), Memory - 0.0B (100.00%)

---
+++ my-leetcode-solutions/0184-department-highest-salary/solution.sql
@@ -0,0 +1,8 @@
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+FROM Employee e
+INNER JOIN Department d ON e.departmentId = d.id
+WHERE (e.departmentId, e.salary) IN (
+    SELECT departmentId, MAX(salary)  -- Step 1: find max salary per dept
+    FROM Employee
+    GROUP BY departmentId
+);
